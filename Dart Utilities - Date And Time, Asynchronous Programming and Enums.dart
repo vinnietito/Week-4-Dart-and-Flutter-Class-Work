10802cc8a81d337a05d683735159c7590aad3719
vinnietito <vincentnjuguna69@gmail.com>
2024-11-18 10:49:33 +0300

    Commits

---
+++ Dart Utilities - Date And Time, Asynchronous Programming and Enums.dart
@@ -0,0 +1,13 @@
+import 'package:intl/intl.dart'; // Import the intl package for date formatting
+
+void main(){
+  //Current date and time
+  DateTime now = DateTime.now();
+  print(('Current date and time: $now'));
+
+  //Creating a specific date and time
+  DateTime specifinDate = DateTime(2024,9,10,14,30);
+  print("'Specific date and time: $specifinDate");
+
+  //Formating date and time
+}
